d62137fd8518689685f28ff2790ffeff8806e533
sanchezlucasar <sanchezlucasar@gmail.com>
2025-09-03 17:58:09 -0300

    Se sube el proyecto

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "node:url";
+
+export default defineConfig({
+    vite: {
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+    },
+});
