10b58b9b2c708eeba56e961878a03999ac6713c4
sanchezlucasar <sanchezlucasar@gmail.com>
2025-09-03 21:02:59 +0000

    Config Astro para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 import { fileURLToPath } from "node:url";
 
 export default defineConfig({
+    site: 'https://sanchezlucasar.github.io',
+    base: '/Astro-sms-page/',
+    outDir: 'dist', // Carpeta donde se genera el build (por defecto "dist")
     vite: {
         resolve: {
             alias: {
